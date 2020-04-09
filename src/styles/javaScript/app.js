dce227d3d84204bacf6de24778bdd17849a1d20e
unknown <eloso85@gmail.com>
2020-04-09 14:13:26 -0500

    finished

--- src/styles/javaScript/app.js
+++ src/styles/javaScript/app.js
@@ -33,11 +33,16 @@ function win(user, computer){
     comScore.innerHTML = computerScore;
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "com".fontsize(3).sup();
-
+    
+    document.getElementById(user).classList.add('green-glow')
     result.innerHTML = `${converToWord(user)}${smallUserWord} beats  ${converToWord(computer)}${smallCompWord}. You win! `
+    setTimeout(function(){document.getElementById(user).classList.remove('green-glow')},1000)
     console.log(user);
     console.log(computer);
+
 }
+
+
 
 function lose(user,computer){
     computerScore++;
@@ -45,20 +50,22 @@ function lose(user,computer){
     comScore.innerHTML = computerScore;
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "com".fontsize(3).sup();
-
+    document.getElementById(user).classList.add('red-glow')
     result.innerHTML = `${converToWord(user)}${smallUserWord} loses  ${converToWord(computer)}${smallCompWord}. You lost! `
+    setTimeout(function(){document.getElementById(user).classList.remove('red-glow')},1000)
     console.log(user);
     console.log(computer);
 }
 
-function draw(){
-    userScore++;
+function draw(user,computer){
+    
     playerScore.innerHTML = userScore;
     comScore.innerHTML = computerScore;
     const smallUserWord = "user".fontsize(3).sup();
     const smallCompWord = "com".fontsize(3).sup();
-
-    result.innerHTML = `${converToWord(user)}${smallUserWord} beats  ${converToWord(computer)}${smallCompWord}. You win! `
+    document.getElementById(user).classList.add('grey-glow')
+    result.innerHTML = `${converToWord(user)}${smallUserWord} equals  ${converToWord(computer)}${smallCompWord}. Its a draw `
+    setTimeout(function(){document.getElementById(user).classList.remove('grey-glow')},1000)
     console.log(user);
     console.log(computer);
 }
